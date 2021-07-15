0dcca7468da269d1ebf0465c25c6ffd7cd1bfce8
giladcochavi <79969856+GiladC@users.noreply.github.com>
2021-07-16 02:40:38 +0300

    Update board.java

--- chess game/src/board.java
+++ chess game/src/board.java
@@ -1,10 +1,25 @@
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
+import java.util.Set;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public class board {
 	public static void main(String[] args) throws IOException{
+		Set<Piece> pieces = Piece.pieces;
+		BufferedImage piecesImage = ImageIO.read(new File("chess_pieces.png"));
+		Image[] allPieces = new Image[12]; //6 pieces for black and 6 pieces for white.
+		int index = 0;
+		for(int i = 0; i < 400; i +=200) {
+			for(int j = 0; j < 1200; j+=200) {
+				allPieces[index] = piecesImage.getSubimage(j, i, 200, 200).getScaledInstance(64, 64, BufferedImage.SCALE_SMOOTH);
+				index++;
+			}
+		}
+		pieces.add(new Piece(3,3, false, PieceType.QUEEN));
 		final JFrame frame = new JFrame();
 		frame.setBounds(10, 10, 525, 549);
 	    JPanel panel = new JPanel() {
@@ -13,7 +28,7 @@ public class board {
 			super.paint(g);
 			for (int y = 0; y < 8; y++) {
 				for (int x = 0; x < 8; x++) {
-					if ((x%2) == (y%2)) { 
+					if ((x%2) == (y%2)) {  //statement check whether this block is white or black
 						g.setColor(Color.white);
 					}
 					else {
@@ -21,6 +36,33 @@ public class board {
 					}
 					g.fillRect(x*64, y*64, 64, 64);
 				}
+			}
+			for (Piece piece : pieces) {
+				int i;
+				switch(piece.type) {
+				case KING:
+					i = 0;
+					break;
+				case QUEEN:
+					i = 1;
+					break;
+				case BISHOP:
+					i = 2;
+					break;
+				case KNIGHT:
+					i = 3;
+					break;
+				case ROOK:
+					i = 4;
+					break;
+				case PAWN:
+					i = 5;
+					break;
+				default: //Effectively unreachable
+					i = 0;
+				}
+				if (!piece.isWhite) { i += 6; }
+				g.drawImage(allPieces[i], piece.x*64, piece.y*64, this);
 			}
 		}
 	};
