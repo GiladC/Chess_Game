c9d932e491289684d79ccdd2695644b497e42e73
giladcochavi <79969856+GiladC@users.noreply.github.com>
2021-07-15 21:50:27 +0300

    Add files via upload

---
+++ chess game/src/board.java
@@ -0,0 +1,30 @@
+
+import java.awt.*;
+import javax.swing.*;
+
+public class board {
+	public static void main(String[] args) {
+		final JFrame frame = new JFrame();
+		frame.setBounds(10, 10, 525, 549);
+	    JPanel panel = new JPanel() {
+		private static final long serialVersionUID = -4396970882832779043L;
+		public void paint(Graphics g) {
+			super.paint(g);
+			for (int y = 0; y < 8; y++) {
+				for (int x = 0; x < 8; x++) {
+					if ((x%2) == (y%2)) { 
+						g.setColor(Color.white);
+					}
+					else {
+						g.setColor(new Color(67, 121, 44));
+					}
+					g.fillRect(x*64, y*64, 64, 64);
+				}
+			}
+		}
+	};
+	frame.add(panel);
+	frame.setDefaultCloseOperation(3);
+	frame.setVisible(true);
+	}
+}
---
+++ chess game/src/pieces.java
@@ -0,0 +1,8 @@
+public class pieces {
+	private int x, y; //coords
+	private boolean isWhite; //whether white piece or not
+	enum PieceType {Pawn, Knight, Bishop, Rook, Queen, King}
+	private PieceType type;
+	
+	
+}
