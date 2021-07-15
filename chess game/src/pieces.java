d11e2afa7b520720bc04d8ce2b1b430081046742
giladcochavi <79969856+GiladC@users.noreply.github.com>
2021-07-15 22:44:02 +0300

    Update pieces.java

--- chess game/src/pieces.java
+++ chess game/src/pieces.java
@@ -1,8 +1,31 @@
-public class pieces {
-	private int x, y; //coords
+import java.util.HashSet;
+import java.util.Set;
+
+public class Piece {
+	private int x, y; //cords
 	private boolean isWhite; //whether white piece or not
-	enum PieceType {Pawn, Knight, Bishop, Rook, Queen, King}
+	enum PieceType {PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING}
 	private PieceType type;
-	
-	
+	static private Set<Piece> pieces = new HashSet<Piece>();
+	public Piece(int x, int y, boolean isWhite, PieceType type) {
+		this.x = x; this.y = y; 
+		this.isWhite = isWhite;
+		this.type = type;
+		pieces.add(this);
+	}
+	public void move(int x, int y) throws Exception{
+		for (Piece piece : pieces) {
+			if (piece.x == this.x && piece.y == this.y) {
+				if (piece.isWhite == this.isWhite) {
+					throw new Exception("You can't kill your own piece.");
+				}
+				this.x = x;	this.y = y;
+				piece.kill();  //kill method will be added later
+			}
+		}
+		this.x = x; this.y = y;
+	}
+	private void kill() {
+		pieces.remove(this);
+	}
 }
