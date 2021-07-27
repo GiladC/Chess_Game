40b14bc53ef5a208a01032991e05fe081acbd752
unknown <GiladCochavi@mail.tau.ac.il>
2021-07-28 02:46:23 +0300

    castling added

--- chess game/src/Piece.java
+++ chess game/src/Piece.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class Piece {
@@ -27,5 +29,237 @@ public class Piece {
 		if (pieceDest != null) {
 			pieces.remove(pieceDest); //killing the piece on destination
 		}
+	}
+	/*ret = 0 iff not legal destination
+	 *ret = 1 iff legal destination and NO enemy piece killed
+	 *ret = 2 iff legal destination and enemy piece killed
+	 *(x,y) are destination coords, isWhite is the white field of the piece.
+     */
+    int checkDest(int x, int y) {  
+   	if( x > 7 || y > 7 || x < 0 || y < 0) { //can't go out of board
+   		return 0;
+   	}
+   	for (Piece piece : pieces) {
+   		if (piece.i == x && piece.j == y) {
+   			if(piece.white == this.white) { return 0; } //can't kill your own piece
+   			return 2;  //kills a piece
+   		}
+
+   	}
+   	return 1; //a possible move without any piece being killed
+    }
+	/*
+	 * generates all destinations possible for the piece
+	 * doesn't include castle for king
+	 */
+	public List<int[]> getDests(){
+		List<int[]> lst = new ArrayList<>();
+		switch(this.type) {	
+		case KING:
+	    	if(checkDest(i, j +1) != 0) { 
+	    	int[] arr = {i, j+1};
+	     	lst.add(arr);}
+	    	if(checkDest(i, j -1) != 0) { 
+	    	int[] arr = {i, j-1};
+	    	lst.add(arr);}
+	    	if(checkDest(i -1, j) != 0) { 
+	    	int[] arr = {i-1, j};
+	    	lst.add(arr);}
+	    	if(checkDest(i +1, j) != 0) {
+	    	int[] arr = {i+1, j};
+	    	lst.add(arr);}
+	    	if(checkDest(i -1, j-1) != 0) { 
+	    	int[] arr = {i-1, j-1};
+	    	lst.add(arr);}
+	    	if(checkDest(i -1, j+1) != 0) {
+		    int[] arr = {i-1, j+1};
+	    	lst.add(arr);}
+	    	if(checkDest(i +1, j+1) != 0) { 
+	    	int[] arr = {i+1, j+1};
+	    	lst.add(arr);}
+	    	if(checkDest(i +1, j-1) != 0) {
+		    int[] arr = {i+1, j-1};
+	    	lst.add(arr);}
+			break;
+		case BISHOP:
+			for (int ip = 1; ip < 8; ip++) {
+				int k = checkDest(i +ip, j + ip);
+				if (k == 0) {break; }
+				int[] arr = {i+ip, j+ip};
+			   	lst.add(arr);
+			   	if (k == 2) { break; }
+			}
+			for (int ip = 1; ip < 8; ip++) {
+				int k = checkDest(i +ip, j - ip);
+				if (k == 0) { break; }
+				int[] arr = {i+ip, j-ip};
+			   	lst.add(arr);
+			   	if (k == 2) { break; }
+			}
+			for (int ip = 1; ip < 8; ip++) {
+				int k = checkDest(i -ip, j - ip);
+				if (k == 0) {break; }
+				int[] arr = {i-ip, j-ip};
+			   	lst.add(arr);
+			   	if (k == 2) { break; }
+			}
+			for (int ip = 1; ip < 8; ip++) {
+				int k = checkDest(i -ip, j + ip);
+				if (k == 0) { break; }
+				int[] arr = {i-ip, j+ip};
+			   	lst.add(arr);
+			   	if (k == 2) { break; }
+			}
+			break;
+		case KNIGHT:
+			if(checkDest(i+2, j +1) != 0) {  
+			int[] arr = {i+2, j+1};
+		   	lst.add(arr);}
+			if(checkDest(i+2, j -1) != 0) { 
+			int[] arr = {i+2, j-1};
+			lst.add(arr);}
+			if(checkDest(i-2, j +1) != 0) {  
+			int[] arr = {i-2, j+1};
+			lst.add(arr);}
+			if(checkDest(i-2, j -1) != 0) { 
+			int[] arr = {i-2, j-1};
+			lst.add(arr);}
+			if(checkDest(i+1, j +2) != 0) {
+			int[] arr = {i+1, j+2};
+			lst.add(arr);}
+			if(checkDest(i-1, j +2) != 0) { 
+			int[] arr = {i-1, j+2};
+			lst.add(arr);}
+			if(checkDest(i+1, j -2) != 0) { 
+			int[] arr = {i+1, j-2};
+			lst.add(arr);}
+			if(checkDest(i-1, j -2) != 0) { 
+			int[] arr = {i-1, j-2};
+			lst.add(arr);}
+			break;
+		case PAWN:
+			if (this.white) { //if its white 
+				if (checkDest(i-1, j-1) == 2) { 
+					int[] arr0 = {i -1, j-1};
+					lst.add(arr0);
+				}
+				if (checkDest(i +1, j -1) == 2) {
+					int[] arr1 = {i +1, j-1};
+					lst.add(arr1);
+				}
+				if(checkDest(i, j -1) != 1) { break;} //can't kill forward
+				int[] arr = {i, j-1}; 
+				lst.add(arr);
+				if(checkDest(i, j-2) == 1 && j == 6) { 
+				int[] arr2 = {i, j-2};
+				lst.add(arr2);}
+			}
+			else { //if its black
+				if (checkDest(i-1, j+1) == 2) {
+					int[] arr0 = {i -1, j+1};
+					lst.add(arr0);
+				}
+				if (checkDest(i +1, j +1) == 2) {
+					int[] arr1 = {i +1, j+1};
+					lst.add(arr1);
+				}
+				if (checkDest(i, j+1) != 1) { break;} //can't attack forward
+				int[] arr = {i, j+1};
+			   	lst.add(arr);
+				if (j == 1 && checkDest(i, j +2) == 1 ) { 
+				int[] arr2 = {i, j+2};
+				lst.add(arr2);}
+			}
+			break;
+		case ROOK:
+			for (int ip = 1; ip < 8; ip++) {
+				int k = checkDest(i, j + ip);
+				if (k == 0) {break; }
+				int[] arr = {i, j+ip};
+				lst.add(arr);
+				if (k == 2) { break; }
+			}
+			for (int ip = 1; ip < 8; ip++) {
+				int k = checkDest(i + ip, j);
+				if (k == 0) { break; }
+				int[] arr = {i+ip, j};
+				lst.add(arr);
+				if (k == 2) { break; }
+			}
+			for (int ip = 1; ip < 8; ip++) {
+				int k = checkDest(i, j - ip);
+				if (k == 0) {break; }
+				int[] arr = {i, j-ip};
+				lst.add(arr);
+				if (k == 2) { break; }
+			}
+			for (int ip = 1; ip < 8; ip++) {
+				int k = checkDest(i -ip, j);
+				if (k == 0) { break; }
+				int[] arr = {i-ip, j};
+				lst.add(arr);
+				if (k == 2) { break; }
+			}
+			break;
+		case QUEEN: //all destinations for queen are all destinations for rook + all destinations for bishop
+			for (int ip = 1; ip < 8; ip++) {
+				int k = checkDest(i +ip, j + ip);
+				if (k == 0) {break; }
+				int[] arr = {i+ip, j+ip};
+			   	lst.add(arr);
+			   	if (k == 2) { break; }
+			}
+			for (int ip = 1; ip < 8; ip++) {
+				int k = checkDest(i +ip, j - ip);
+				if (k == 0) { break; }
+				int[] arr = {i+ip, j-ip};
+			   	lst.add(arr);
+			   	if (k == 2) { break; }
+			}
+			for (int ip = 1; ip < 8; ip++) {
+				int k = checkDest(i -ip, j - ip);
+				if (k == 0) {break; }
+				int[] arr = {i-ip, j-ip};
+			   	lst.add(arr);
+			   	if (k == 2) { break; }
+			}
+			for (int ip = 1; ip < 8; ip++) {
+				int k = checkDest(i -ip, j + ip);
+				if (k == 0) { break; }
+				int[] arr = {i-ip, j+ip};
+			   	lst.add(arr);
+			   	if (k == 2) { break; }
+			}
+			for (int ip = 1; ip < 8; ip++) {
+				int k = checkDest(i, j + ip);
+				if (k == 0) {break; }
+				int[] arr = {i, j+ip};
+				lst.add(arr);
+				if (k == 2) { break; }
+			}
+			for (int ip = 1; ip < 8; ip++) {
+				int k = checkDest(i + ip, j);
+				if (k == 0) { break; }
+				int[] arr = {i+ip, j};
+				lst.add(arr);
+				if (k == 2) { break; }
+			}
+			for (int ip = 1; ip < 8; ip++) {
+				int k = checkDest(i, j - ip);
+				if (k == 0) {break; }
+				int[] arr = {i, j-ip};
+				lst.add(arr);
+				if (k == 2) { break; }
+			}
+			for (int ip = 1; ip < 8; ip++) {
+				int k = checkDest(i -ip, j);
+				if (k == 0) { break; }
+				int[] arr = {i-ip, j};
+				lst.add(arr);
+				if (k == 2) { break; }
+			}
+			break;
+		}
+		return lst;
 	}
 }
--- chess game/src/board.java
+++ chess game/src/board.java
@@ -4,7 +4,6 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -15,8 +14,12 @@ import javax.swing.*;
 
 public class board {
     static Piece currPiece = null; //the piece that was last clicked on
-	private static List<int[]> legalDests; //all the legal destinations for the current piece, in cords (x,y)
+	private static List<int[]> legalDests = new ArrayList<>(); //all the legal destinations for the current piece, in cords (x,y)
 	static boolean Wlose = false, Blose = false; //either white or black lost
+	static boolean Wking = false, Bking = false; //either white or black king has moved, relevant to castle
+	//either white or black rooks moved, relevant to castle. the [0] entry for the rook with y=0, and [1] for y=7.
+	static boolean[] Wrook = {false, false}, Brook = {false, false}; 
+	static boolean castle = false; //this boolean just to simplify stuff
 	public static void main(String[] args) throws IOException{
 		legalDests = new ArrayList<>();
 		Set<Piece> pieces = Piece.pieces;
@@ -63,22 +66,30 @@ public class board {
 		frame.setBounds(10, 10, 525, 549);
 	    JPanel panel = new JPanel() {	    	
 		private static final long serialVersionUID = -4396970882832779043L;
-		/*ret = 0 iff not legal destination
-		  ret = 1 iff legal destination and NO enemy piece killed
-		  ret = 2 iff legal destination and enemy piece killed
-		  */
-        int checkDest(int x, int y) { 
-	   	if( x > 7 || y > 7 || x < 0 || y < 0) { //can't go out of board
-	   		return 0;
-	   	}
-	   	for (Piece piece : pieces) {
-	   		if (piece.i == x && piece.j == y) {
-	   			if(piece.white == currPiece.white) { return 0; } //can't kill your own piece
-	   			return 2;  //kills a piece
-	   		}
-	   	}
-	   	return 1; //a possible move without any piece being killed
+        /*
+         *checks whether the block in position (x, y) with the color IsWhite, threatened by an opposite piece
+         */
+        boolean threatened(int x, int y, boolean isWhite) {
+        	for (Piece p : pieces) {
+        		if (p.white != isWhite) {
+        			for (int[] arr : p.getDests()) {
+        				if (arr[0] == x && arr[1] == y) {
+        					return true;
+        				}
+        			}
+        		}
+        	}
+        	return false;
         }
+        //get piece for mouse coords pressed
+        public Piece getPiece(int x, int y) {		
+			for (Piece piece : pieces) {
+				if (x == piece.i && y == piece.j) {
+					return piece;
+				}
+			}
+			return null;
+		}
 		public void paint(Graphics g) {
 			super.paint(g);
 			for (int y = 0; y < 8; y++) {
@@ -93,239 +104,62 @@ public class board {
 				}
 			}
 			if (currPiece != null) {
-				int xp = currPiece.i; int yp = currPiece.j;
 				g.setColor(new Color(51, 0, 103));
-				g.fillRect(64*xp, 64*yp, 64, 64); //the color that signs the piece that has been chosen
+				g.fillRect(64*currPiece.i, 64*currPiece.j, 64, 64); //the color that signs the piece that has been chosen
 				g.setColor(new Color(51,255,255)); //the color of the places the piece can go
-				switch(currPiece.type) {
-				case KING: //all the moves possible for a king 
-					if(checkDest(xp, yp +1) != 0) { g.fillRect(1+64*xp,1+ 64*(yp+1), 62, 62); 
-					int[] arr = {xp, yp+1};
-				   	legalDests.add(arr);}
-					if(checkDest(xp, yp -1) != 0) {g.fillRect(1+64*xp, 1+64*(yp-1), 62, 62); 
-					int[] arr = {xp, yp-1};
-				   	legalDests.add(arr);}
-					if(checkDest(xp -1, yp) != 0) {g.fillRect(1+64*(xp-1),1+ 64*yp, 62, 62); 
-					int[] arr = {xp-1, yp};
-				   	legalDests.add(arr);}
-					if(checkDest(xp +1, yp) != 0) {g.fillRect(1+64*(xp+1), 1+64*yp, 62, 62); 
-					int[] arr = {xp+1, yp};
-				   	legalDests.add(arr);}
-					if(checkDest(xp -1, yp-1) != 0) {g.fillRect(1+64*(xp-1),1+ 64*(yp-1), 62, 62); 
-					int[] arr = {xp-1, yp-1};
-				   	legalDests.add(arr);}
-					if(checkDest(xp -1, yp+1) != 0) {g.fillRect(1+64*(xp-1), 1+64*(yp+1), 62, 62); 
-					int[] arr = {xp-1, yp+1};
-				   	legalDests.add(arr);}
-					if(checkDest(xp +1, yp+1) != 0) {g.fillRect(1+64*(xp+1), 1+64*(yp+1), 62, 62); 
-					int[] arr = {xp+1, yp+1};
-				   	legalDests.add(arr);}
-					if(checkDest(xp +1, yp-1) != 0) {g.fillRect(1+64*(xp+1), 1+64*(yp-1), 62, 62); 
-					int[] arr = {xp+1, yp-1};
-				   	legalDests.add(arr);}
-					break;
-				case BISHOP: //all the moves possible for a bishop
-					for (int i = 1; i < 8; i++) {
-						int k = checkDest(xp +i, yp + i);
-						if (k == 0) {break; }
-						g.fillRect(1+64*(xp+i), 1+64*(yp+i), 62, 62);
-						int[] arr = {xp+i, yp+i};
-					   	legalDests.add(arr);
-					   	if (k == 2) { break; }
-					}
-					for (int i = 1; i < 8; i++) {
-						int k = checkDest(xp +i, yp - i);
-						if (k == 0) { break; }
-						g.fillRect(1+64*(xp +i),1+ 64*(yp-i), 62, 62);
-						int[] arr = {xp+i, yp-i};
-					   	legalDests.add(arr);
-					   	if (k == 2) { break; }
-					}
-					for (int i = 1; i < 8; i++) {
-						int k = checkDest(xp -i, yp - i);
-						if (k == 0) {break; }
-						g.fillRect(1+64*(xp-i), 1+64*(yp-i), 62, 62);
-						int[] arr = {xp-i, yp-i};
-					   	legalDests.add(arr);
-					   	if (k == 2) { break; }
-					}
-					for (int i = 1; i < 8; i++) {
-						int k = checkDest(xp -i, yp + i);
-						if (k == 0) { break; }
-						g.fillRect(1+64*(xp -i),1+ 64*(yp+i), 62, 62);
-						int[] arr = {xp-i, yp+i};
-					   	legalDests.add(arr);
-					   	if (k == 2) { break; }
-					}
-					break;
-				case KNIGHT: //all the moves possible for a knight
-					if(checkDest(xp+2, yp +1) != 0) { g.fillRect(1+64*(xp+2),1+ 64*(yp+1), 62, 62); 
-					int[] arr = {xp+2, yp+1};
-				   	legalDests.add(arr);}
-					if(checkDest(xp+2, yp -1) != 0) { g.fillRect(1+64*(xp+2),1+ 64*(yp-1), 62, 62); 
-					int[] arr = {xp+2, yp-1};
-				   	legalDests.add(arr);}
-					if(checkDest(xp-2, yp +1) != 0) { g.fillRect(1+64*(xp-2),1+ 64*(yp+1), 62, 62); 
-					int[] arr = {xp-2, yp+1};
-				   	legalDests.add(arr);}
-					if(checkDest(xp-2, yp -1) != 0) { g.fillRect(1+64*(xp-2),1+ 64*(yp-1), 62, 62); 
-					int[] arr = {xp-2, yp-1};
-				   	legalDests.add(arr);}
-					if(checkDest(xp+1, yp +2) != 0) { g.fillRect(1+64*(xp+1),1+ 64*(yp+2), 62, 62); 
-					int[] arr = {xp+1, yp+2};
-				   	legalDests.add(arr);}
-					if(checkDest(xp-1, yp +2) != 0) { g.fillRect(1+64*(xp-1),1+ 64*(yp+2), 62, 62); 
-					int[] arr = {xp-1, yp+2};
-				   	legalDests.add(arr);}
-					if(checkDest(xp+1, yp -2) != 0) { g.fillRect(1+64*(xp+1),1+ 64*(yp-2), 62, 62); 
-					int[] arr = {xp+1, yp-2};
-				   	legalDests.add(arr);}
-					if(checkDest(xp-1, yp -2) != 0) { g.fillRect(1+64*(xp-1),1+ 64*(yp-2), 62, 62); 
-					int[] arr = {xp-1, yp-2};
-				   	legalDests.add(arr);}
-					break;
-				case PAWN:  //all the moves possible for a pawn
-					if (currPiece.white) { //if its white 
-						if (checkDest(xp-1, yp-1) == 2) { 
-							g.fillRect(1+64*(xp-1), 1+64*(yp-1), 62, 62);
-							int[] arr0 = {xp -1, yp-1};
-							legalDests.add(arr0);
+				List<int[]> lst = currPiece.getDests();
+				int[] arr;
+				for (int i = 0; i < lst.size(); i++) { //going through all the possible destinations
+					arr = lst.get(i);
+					if(currPiece.type == PieceType.KING) {
+						if (!threatened(arr[0], arr[1], currPiece.white)) {
+						g.fillRect(1+64*arr[0], 1+64*arr[1], 62, 62);
+						legalDests.add(arr);
 						}
-						if (checkDest(xp +1, yp -1) == 2) {
-							g.fillRect(1+64*(xp+1), 1+64*(yp-1), 62, 62);
-							int[] arr1 = {xp +1, yp-1};
-							legalDests.add(arr1);
+					}
+					else {
+						g.fillRect(1+64*arr[0], 1+64*arr[1], 62, 62);
+						legalDests.add(arr);
+			    	}
+				}
+				//next checking for castle option
+				if(currPiece.type == PieceType.KING) {
+					if (currPiece.white && !Wking) {
+						if (!Wrook[0]) {
+							if (getPiece(3, 7) == null && getPiece(2, 7) == null &&
+									!threatened(2,7, true) && getPiece(1,7) == null &&
+									!threatened(3,7,true) && !threatened(4,7,true)) {
+								castle = true; g.fillRect(1 + 2*64, 1 + 7*64, 62, 62);
+								int[] arr2 = {2, 7}; legalDests.add(arr2);
+							}
 						}
-						int k = checkDest(xp, yp -1);
-						if(k != 1) { break;} //can't kill in a straight line
-						g.fillRect(1+64*xp,1+ 64*(yp-1), 62, 62);
-						int[] arr = {xp, yp-1}; 
-					   	legalDests.add(arr);
-						if(checkDest(xp, yp-2) == 1 && yp == 6) { g.fillRect(1+64*xp, 1+64*(yp-2), 62, 62); 
-						int[] arr2 = {xp, yp-2};
-					   	legalDests.add(arr2);}
-					}
-					else { //if its black
-						if (checkDest(xp-1, yp+1) == 2) {
-							g.fillRect(1+64*(xp-1), 1+64*(yp+1), 62, 62);
-							int[] arr0 = {xp -1, yp+1};
-							legalDests.add(arr0);
+						if (!Wrook[1]) {
+							if (getPiece(5, 7) == null && getPiece(6, 7) == null
+									&& !threatened(4,7, true) && 
+									!threatened(5,7,true) && !threatened(6,7,true)) {
+								castle = true; g.fillRect(1 + 6*64, 1 + 7*64, 62, 62);
+								int[] arr2 = {6, 7}; legalDests.add(arr2);
+							}
 						}
-						if (checkDest(xp +1, yp +1) == 2) {
-							g.fillRect(1+64*(xp+1), 1+64*(yp+1), 62, 62);
-							int[] arr1 = {xp +1, yp+1};
-							legalDests.add(arr1);
+					}
+					if (!currPiece.white && !Bking) {
+						if (!Brook[0]) {
+							if (getPiece(3, 0) == null && getPiece(2,0) == null 
+									&& !threatened(2,0, false) && getPiece(1,0) == null &&
+									!threatened(3,0,false) && !threatened(4,0,false)) {
+								castle = true; g.fillRect(1 + 2*64, 1 + 0*64, 62, 62);
+								int[] arr2 = {2, 0}; legalDests.add(arr2);
+							}
 						}
-						int k = checkDest(xp, yp + 1);
-						if (k != 1) { break;}
-						g.fillRect(1+64*xp, 1+64*(yp+1), 62, 62); 
-						int[] arr = {xp, yp+1};
-					   	legalDests.add(arr);
-						if (yp == 1 && checkDest(xp, yp +2) != 0 && k == 1) { g.fillRect(1+64*xp, 1+64*(yp+2), 62, 62); 
-						int[] arr2 = {xp, yp+2};
-					   	legalDests.add(arr2);}
+						if (!Brook[1]) {
+							if (getPiece(5, 0) == null && getPiece(6,0) == null  
+									&& !threatened(4,0, false) && 
+									!threatened(5,0,false) && !threatened(6,0,false)) {
+								castle = true; g.fillRect(1 + 6*64, 1 + 0*64, 62, 62);
+								int[] arr2 = {6, 0}; legalDests.add(arr2);
+							}
+						}
 					}
-					break;
-				case QUEEN: //the moves possile for  a queen or the moves possible for a rook +  bishop 
-					for (int i = 1; i < 8; i++) {
-						int k = checkDest(xp, yp + i);
-						if (k == 0) {break; }
-						g.fillRect(1+64*xp, 1+64*(yp+i), 62, 62);
-						int[] arr = {xp, yp+i};
-					   	legalDests.add(arr);
-						if (k == 2) { break; }
-					}
-					for (int i = 1; i < 8; i++) {
-						int k = checkDest(xp + i, yp);
-						if (k == 0) { break; }
-						g.fillRect(1+64*(xp +i),1+ 64*yp, 62, 62);
-						int[] arr = {xp+i, yp};
-					   	legalDests.add(arr);
-						if (k == 2) { break; }
-					}
-					for (int i = 1; i < 8; i++) {
-						int k = checkDest(xp, yp - i);
-						if (k == 0) {break; }
-						g.fillRect(1+64*xp, 1+64*(yp-i), 62, 62);
-						int[] arr = {xp, yp-i};
-					   	legalDests.add(arr);
-						if (k == 2) { break; }
-					}
-					for (int i = 1; i < 8; i++) {
-						int k = checkDest(xp -i, yp);
-						if (k == 0) { break; }
-						g.fillRect(1+64*(xp -i),1+ 64*yp, 62, 62);
-						int[] arr = {xp-i, yp};
-					   	legalDests.add(arr);
-						if (k == 2) { break; }
-					}
-					for (int i = 1; i < 8; i++) {
-						int k = checkDest(xp +i, yp + i);
-						if (k == 0) {break; }
-						g.fillRect(1+64*(xp+i), 1+64*(yp+i), 62, 62);
-						int[] arr = {xp+i, yp+i};
-					   	legalDests.add(arr);
-					   	if (k == 2) { break; }
-					}
-					for (int i = 1; i < 8; i++) {
-						int k = checkDest(xp +i, yp - i);
-						if (k == 0) { break; }
-						g.fillRect(1+64*(xp +i),1+ 64*(yp-i), 62, 62);
-						int[] arr = {xp+i, yp-i};
-					   	legalDests.add(arr);
-					   	if (k == 2) { break; }
-					}
-					for (int i = 1; i < 8; i++) {
-						int k = checkDest(xp -i, yp - i);
-						if (k == 0) {break; }
-						g.fillRect(1+64*(xp-i), 1+64*(yp-i), 62, 62);
-						int[] arr = {xp-i, yp-i};
-					   	legalDests.add(arr);
-					   	if (k == 2) { break; }
-					}
-					for (int i = 1; i < 8; i++) {
-						int k = checkDest(xp -i, yp + i);
-						if (k == 0) { break; }
-						g.fillRect(1+64*(xp -i),1+ 64*(yp+i), 62, 62);
-						int[] arr = {xp-i, yp+i};
-					   	legalDests.add(arr);
-					   	if (k == 2) { break; }
-					}
-					break;
-				case ROOK:  //all the moves possible for a rook
-					for (int i = 1; i < 8; i++) {
-						int k = checkDest(xp, yp + i);
-						if (k == 0) {break; }
-						g.fillRect(1+64*xp, 1+64*(yp+i), 62, 62);
-						int[] arr = {xp, yp+i};
-					   	legalDests.add(arr);
-						if (k == 2) { break; }
-					}
-					for (int i = 1; i < 8; i++) {
-						int k = checkDest(xp + i, yp);
-						if (k == 0) { break; }
-						g.fillRect(1+64*(xp +i),1+ 64*yp, 62, 62);
-						int[] arr = {xp+i, yp};
-					   	legalDests.add(arr);
-						if (k == 2) { break; }
-					}
-					for (int i = 1; i < 8; i++) {
-						int k = checkDest(xp, yp - i);
-						if (k == 0) {break; }
-						g.fillRect(1+64*xp, 1+64*(yp-i), 62, 62);
-						int[] arr = {xp, yp-i};
-					   	legalDests.add(arr);
-						if (k == 2) { break; }
-					}
-					for (int i = 1; i < 8; i++) {
-						int k = checkDest(xp -i, yp);
-						if (k == 0) { break; }
-						g.fillRect(1+64*(xp -i),1+ 64*yp, 62, 62);
-						int[] arr = {xp-i, yp};
-					   	legalDests.add(arr);
-						if (k == 2) { break; }
-					}
-					break;
 				}
 			}
 			for (Piece piece : pieces) {
@@ -356,7 +190,7 @@ public class board {
 			Wlose = true; Blose = true;
 			for (Piece p : pieces) { //checks whether white or black lost
 				if (p.type == PieceType.KING) {
-					if (p.white) { Wlose = false; }
+					if (p.white) { Wlose = false;  }
 					else { Blose = false; }
 				}
 			}
@@ -373,9 +207,10 @@ public class board {
 		}
 	};
 	frame.addMouseListener(new MouseListener() {
-		public Piece getPiece(int x, int y) {		//get piece for mouse cords pressed
+		//get piece for mouse coords pressed
+		public Piece getPiece(int x, int y) {		
 			for (Piece piece : pieces) {
-				if (Math.floor((x-7)/64) == piece.i && Math.floor((y-30)/64) == piece.j) {
+				if (x == piece.i && y == piece.j) {
 					return piece;
 				}
 			}
@@ -385,32 +220,65 @@ public class board {
 		public void mouseClicked(MouseEvent e) {
 			// TODO Auto-generated method stub
 		}
-		boolean whiteTurn = true; //White moves first at chess
+		boolean whiteTurn = true; //white moves first at chess
 		@Override
 		public void mousePressed(MouseEvent e) {
 			if (Wlose || Blose) { return; } //the game doesn't continue after someone lost
+			int x =  (int) Math.floor((e.getX()-7)/64);
+			int y = (int) Math.floor((e.getY()-30)/64);
 			if (currPiece == null) { 
-				Piece p = getPiece(e.getX(), e.getY());
+				Piece p = getPiece(x, y);
 				if (p != null) {
 					if (p.white == whiteTurn) {
 						currPiece = p; //updating the current piece
 					}
 				}
-			}	
-			else { //moving the piece to the block
-				int x =  (int) Math.floor((e.getX()-7)/64);
-				int y = (int) Math.floor((e.getY()-30)/64);
+			}
+			/*this statement checks for castle option. castle == true iff castling is an actual option 
+			 *(the square is blue) and the destination coords are the destinations that correspond with castle.
+			 */
+			else if (castle && (x == 2 || x == 6) && (y == 7 || y == 0)) {
 				for (int[] arr : legalDests) {
 					if (x == arr[0] && y == arr[1]) {
 						try {
 							currPiece.move(x, y);
+							if (x == 2) { getPiece(0, y).move(3, y); }
+							if (x == 6) { getPiece(7, y).move(5, y); }
 							whiteTurn = !whiteTurn;
-						} catch (Exception e1) {
-							System.out.println("Problem");
+							if (currPiece.white) { Wking = true; }
+							else { Bking = true; }	
+						}
+						catch (Exception e1) {
+							e1.printStackTrace();
+							System.out.println("an error has occurred while trying to castle");
 						}
 					}
 				}
-				currPiece = null; legalDests.clear();
+				currPiece = null; legalDests.clear(); castle = false;
+			}  
+			else { //moving the piece to the block
+				for (int[] arr : legalDests) {
+					if (x == arr[0] && y == arr[1]) {
+						try {
+							if (currPiece.type == PieceType.ROOK) { //when a rook moves from its starting positin
+								if (currPiece.i == 0 && currPiece.j == 0) { Brook[0] = true; }
+								if (currPiece.i == 7 && currPiece.j == 0) { Brook[1] = true; }
+								if (currPiece.i == 0 && currPiece.j == 7) { Wrook[0] = true; }
+								if (currPiece.i == 7 && currPiece.j == 7) { Wrook[1] = true; }
+							}
+							currPiece.move(x, y);
+							whiteTurn = !whiteTurn;
+							if (currPiece.type == PieceType.KING) {
+								if (currPiece.white) { Wking = true; }
+								else { Bking = true; }
+							}
+						}
+							catch (Exception e1) {
+							System.out.println("an error has occurred");
+						}
+					}
+				}
+				currPiece = null; legalDests.clear(); castle = false;
 			}
 		}
 
